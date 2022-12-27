181e784eb1c419c9f70ebab29239a8d0ff4c5c96
Mark Okhman <mark@okhmans.pro>
2022-12-27 14:22:04 +0100

    fix: cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,7 @@
 import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
-import NonSSRWrapper from "../shared/NoSSR";
-import { DeLabWrapper } from "../shared/DeLabWrapper";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    // <NonSSRWrapper>
-    // <DeLabWrapper>
-    <Component {...pageProps} />
-    // </DeLabWrapper>
-    // </NonSSRWrapper>
-  );
+  return <Component {...pageProps} />;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -64,7 +64,7 @@ export default function Home() {
           </button>
         </>
       ) : (
-        DeLabButton
+        <div className='w-[200px]'>{DeLabButton}</div>
       )}
     </div>
   );
